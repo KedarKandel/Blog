916eb70d792926ee1e29d177fc2df019f0a1739b
KedarKandel <kandelkedar07@gmail.com>
2024-02-29 00:23:56 +0200

    added hamburger menu layout with toggle on click

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,14 +1,20 @@
+import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
 import { AppDispatch, RootState } from "../redux/store";
 import { logoutUserAsync } from "../redux/reducers/userSlice";
 import { validateToken } from "../api-client";
 import { showToast } from "../redux/reducers/toastSlice";
+import { Menu } from "lucide-react";
+import { X } from "lucide-react";
 
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   const isLoggedIn = useSelector((state: RootState) => state.user.isLoggedIn);
   const dispatch = useDispatch<AppDispatch>();
   const navigate = useNavigate();
+
   const handleLogout = async () => {
     await dispatch(logoutUserAsync());
     dispatch(
@@ -16,15 +22,29 @@ const Header = () => {
     );
     navigate("/");
   };
+
+  const handleMenuClick = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <div className="bg-blue-800 py-6">
-      <div className=" container mx-auto flex justify-between">
-        <span className=" text-2xl text-white font-bold tracking-tight">
+      <div className="container mx-auto flex justify-between items-center">
+        <span className="text-2xl text-white font-bold tracking-tight">
           <Link to={"/"} onClick={() => validateToken()}>
             EternaThoughts
           </Link>
         </span>
-        <span className=" flex space-x-2">
+
+        {/* Hamburger menu icon */}
+        <button
+          onClick={handleMenuClick}
+          className=" md:hidden z-10 text-white text-2xl"
+        >
+          {isMenuOpen ? <Menu size="32px" /> : <X size="32px" />}
+        </button>
+
+        <span className="  space-x-2 hidden md:flex">
           {isLoggedIn ? (
             <div className="hidden md:flex items-center gap-8 text-white text-2xl">
               <Link to="/create">Create</Link>
